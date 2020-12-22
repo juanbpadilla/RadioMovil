9fad59e1df86bc412ddf8c7a7b5c3f8d13caa2c1
juanbpadilla <juanbpadillac@gmail.com>
2020-12-21 23:53:12 -0400

    home change

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+
+
+@section('content')
+    <h1>Home</h1>
+@stop
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <header>
+        <ul>
+            <li><a href="#">Novedades</a></li>
+            <li><a href="#">Noticias</a></li>
+            <li><a href="#">Registrarse</a></li>
+        </ul>
+    </header>
+
+    @yield('contend')
+
+    <footer>
+        <p class="copy">&copy; Padilla C., Juan Bautista - 2020 - © Todos los derechos reservados</p>
+    </footer>
+
+</body>
+</html>
